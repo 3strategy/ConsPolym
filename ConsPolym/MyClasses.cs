09891d54c5ed952f55287c047ed7e7fdf5d0bc7b
3strategy <3strategy@gmail.com>
2025-12-05 17:10:42 +0200

    Add project files.

---
+++ ConsPolym/MyClasses.cs
@@ -0,0 +1,67 @@
+﻿
+namespace ConsPolym;
+
+
+public class ClothingItem
+{
+  public virtual void PrintSpecial()
+  => Console.WriteLine("So special");
+
+
+  private string color;
+  private string fabric;
+
+  public ClothingItem(string color, string fabric)
+  {
+    this.color = color;
+    this.fabric = fabric;
+  }
+
+  public string GetFabric() => fabric;
+
+  public void SetFabric(string value) => fabric = value;
+
+  public string GetColor() => color;
+
+  public void SetColor(string value) => color = value;
+
+
+
+}
+public class Shirt : ClothingItem
+{
+  public override void PrintSpecial()
+  => Console.WriteLine("Very special");
+
+
+  private char size;
+  public Shirt(string color, string fabric, char size)
+      : base(color, fabric)
+  {
+    this.size = size;
+  }
+  public char GetSize() => size;
+
+  public void SetSize(char value) => size = value;
+
+
+}
+
+public class Pants : ClothingItem
+{
+  public override void PrintSpecial()
+  => Console.WriteLine("Lovely & special");
+
+  private int size;
+
+  public Pants(string color, string fabric, int size)
+      : base(color, fabric)
+  {
+    this.size = size;
+  }
+  public int GetSize() => size;
+
+  public void SetSize(int value) => size = value;
+
+
+}
---
+++ ConsPolym/TestClothes.cs
@@ -0,0 +1,57 @@
+﻿using ConsPolym;
+
+namespace ConsPolym
+{
+  public class TestClothes
+  {
+    static void Main(string[] args)
+    {
+
+      ClothingItem[] clothingItems = // 2023 syntax
+      [
+        new ClothingItem(color: "blue", fabric: "cotton"),
+        new Shirt(color: "blue", fabric: "cotton",size:'s'),
+        new Pants(color: "black", fabric: "jeans", size: 36),
+        new ClothingItem(color: "pink", fabric: "silk"),
+        new Shirt(color: "green", fabric: "cotton", size: 'm'),
+      ];
+
+      foreach (var item in clothingItems)
+        item.PrintSpecial();
+
+      Pants p = new Pants("blue", "jeans", 36);
+      Shirt sh1 = new Shirt("green", "silk", 'S');
+      ClothingItem cl_p = p;
+      ClothingItem cl_sh1 = sh1;
+
+      string f1 = cl_p.GetFabric();
+      int size3 = sh1.GetSize();
+      //int size3b = cl_sh1.GetSize();
+      char ch5 = sh1.GetSize();
+      //int size4 = cl_p.GetSize();
+      //char ch6 = cl_p.GetSize();
+      string st2=p.GetFabric();
+      //int size1 = p.GetSize();
+      //int size2 = c.GetSize();
+      //Shirt s = new Shirt(color: "green", fabric: "silk", size: 's');
+      //PrintColorAndFabric(s);
+
+
+      int size;
+      if (cl_p is Pants pants) // 2017 pattern matching
+        size = pants.GetSize();
+
+      if (cl_p is Pants)
+        size = ((Pants)cl_p).GetSize();
+
+    }
+
+    public static void PrintColorAndFabric(Shirt shirt)
+    {
+      Console.WriteLine(shirt.GetColor());
+      Console.WriteLine(shirt.GetFabric());
+    }
+  }
+}
+
+
